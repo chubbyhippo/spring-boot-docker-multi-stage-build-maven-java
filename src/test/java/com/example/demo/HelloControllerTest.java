c58edac063fd9c8f41144fc5d5faf00356f55547
ch <mkmsacc@outlook.com>
2022-03-09 08:08:11 +0700

    Add Controller test

---
+++ src/test/java/com/example/demo/HelloControllerTest.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class HelloControllerTest {
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Test
+    void ShouldGetHello() {
+
+        webTestClient.get()
+                .uri("/")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .expectBody(String.class)
+                .isEqualTo("hello");
+
+    }
+}
