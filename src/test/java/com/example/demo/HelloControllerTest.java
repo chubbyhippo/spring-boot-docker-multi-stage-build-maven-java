8bf846386addd3a033b757fded2799fd2c24b2a7
ch <mkmsacc@outlook.com>
2022-11-28 21:53:19 +0700

    Refactor test

--- src/test/java/com/example/demo/HelloControllerTest.java
+++ src/test/java/com/example/demo/HelloControllerTest.java
@@ -6,19 +6,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class HelloControllerTest {
-
-    @Autowired
-    WebTestClient webTestClient;
-
+class HelloTest {
     @Test
-    void ShouldGetHello() {
+    void ShouldGetHello(@Autowired WebTestClient webTestClient) {
 
         webTestClient.get()
                 .uri("/")
                 .exchange()
                 .expectStatus()
-                .is2xxSuccessful()
+                .isOk()
                 .expectBody(String.class)
                 .isEqualTo("hello");
 
